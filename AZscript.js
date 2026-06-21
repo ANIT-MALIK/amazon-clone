c3e2d822559df7c0fa31ace95d6a6bd2efd99062
Anit Malik <anit.25b01011180@abes.ac.in>
2026-06-21 10:53:36 +0530

    Amazon site cloned

---
+++ AZscript.js
@@ -0,0 +1,13 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCart();
+}
+
+function updateCart() {
+  document.getElementById("cart-count").innerText = cart.length;
+}
+
+updateCart();
